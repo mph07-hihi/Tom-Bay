774f4a6fdfffdb4556f539561ce847c2b46996b7
Nguyễn Thị Minh Phương <24021308@vnu.edu.vn>
2026-07-21 14:03:32 +0700

    Thêm chuyển động của Tom (đập cánh)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
 #include <iostream>
+#include <vector>
 
 SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path) {
     SDL_Surface* surface = IMG_Load(path);
@@ -25,13 +26,20 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SDL_Texture* tom1 = loadTexture(renderer, "assets/tom1.png");
+    std::vector<SDL_Texture*> birdFrames;
+    birdFrames.push_back(loadTexture(renderer, "assets/tom1.png"));
+    birdFrames.push_back(loadTexture(renderer, "assets/tom2.png"));
+    birdFrames.push_back(loadTexture(renderer, "assets/tom3.png"));
 
     SDL_FRect birdRect;
     birdRect.x = 150.0f;
     birdRect.y = 100.0f;
     birdRect.w = 300.0f;
     birdRect.h = 210.0f;
+
+    int currentFrame = 0;
+    Uint64 lastFrameTime = SDL_GetTicks();
+    const Uint64 frameDelay = 100;
 
     bool isRunning = true;
     SDL_Event event;
@@ -43,18 +51,26 @@ int main(int argc, char* argv[]) {
             }
         }
 
+        Uint64 currentTime = SDL_GetTicks();
+        if (currentTime - lastFrameTime >= frameDelay) {
+            currentFrame = (currentFrame + 1) % birdFrames.size();
+            lastFrameTime = currentTime;
+        }
+
         SDL_SetRenderDrawColor(renderer, 100, 150, 255, 255);
         SDL_RenderClear(renderer);
 
-        if (tom1) {
-            SDL_RenderTexture(renderer, tom1, nullptr, &birdRect);
+        if (!birdFrames.empty() && birdFrames[currentFrame]) {
+            SDL_RenderTexture(renderer, birdFrames[currentFrame], nullptr, &birdRect);
         }
 
         SDL_RenderPresent(renderer);
         SDL_Delay(16);
     }
 
-    SDL_DestroyTexture(tom1);
+    for (SDL_Texture* tex : birdFrames) {
+        if (tex) SDL_DestroyTexture(tex);
+    }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
