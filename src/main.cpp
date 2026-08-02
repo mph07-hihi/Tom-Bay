6597005916d113299e9b6e2e6b3b541cb299408a
Nguyễn Thị Minh Phương <24021308@vnu.edu.vn>
2026-08-02 21:43:46 +0700

    thêm vận tốc, trọng lực, lực cản, sk phím

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include <iostream>
 #include <vector>
 
+struct bird {
+    int currentFrame;
+    float x, y;
+	float width, height;
+    float velocity, gravity, lift;
+};
+
 SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path) {
     SDL_Surface* surface = IMG_Load(path);
     if (!surface) return nullptr;
@@ -37,30 +44,55 @@ int main(int argc, char* argv[]) {
     birdFrames.push_back(loadTexture(renderer, "assets/tom2.png"));
     birdFrames.push_back(loadTexture(renderer, "assets/tom3.png"));
 
-    SDL_FRect birdRect;
-    birdRect.x = 200.0f;
-    birdRect.y = 150.0f;
-    birdRect.w = 220.0f;
-    birdRect.h = 210.0f;
+    bird tom;
+    tom.currentFrame = 0;
+    tom.x = 200.0f;
+    tom.y = 150.0f;
+    tom.width = 220.0f;
+    tom.height = 210.0f;
+    tom.velocity = 0.0f;
+    tom.gravity = 900.0f;
+    tom.lift = -400.0f;
 
-    int currentFrame = 0;
+	int currentFrame = 0;
     Uint64 lastFrameTime = SDL_GetTicks();
     const Uint64 frameDelay = 100;
-
    
+    Uint64 lastTime = SDL_GetTicks();
+
     bool isRunning = true;
     SDL_Event event;
 
     while (isRunning) {
+        Uint64 currentTimePhysics = SDL_GetTicks();
+        float deltaTime = (currentTimePhysics - lastTime) / 1000.0f;
+        lastTime = currentTimePhysics;
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 isRunning = false;
             }
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                if (event.key.key == SDLK_SPACE) {
+                    tom.velocity = tom.lift;
+                }
+            }
+        }
+        tom.velocity += tom.gravity * deltaTime;
+        tom.y += tom.velocity * deltaTime;
+
+        if (tom.y < 0.0f) {
+            tom.y = 0.0f;
+            tom.velocity = 0.0f;
+        }
+        if (tom.y > 700.0f - tom.height) {
+            tom.y = 700.0f - tom.height;
+            tom.velocity = 0.0f;
         }
 
         Uint64 currentTime = SDL_GetTicks();
         if (currentTime - lastFrameTime >= frameDelay) {
-            currentFrame = (currentFrame + 1) % birdFrames.size();
+            tom.currentFrame = (tom.currentFrame + 1) % birdFrames.size();
             lastFrameTime = currentTime;
         }
 
@@ -71,8 +103,13 @@ int main(int argc, char* argv[]) {
             SDL_RenderTexture(renderer, bgTexture, nullptr, &bgRect);
         }
 
-        if (!birdFrames.empty() && birdFrames[currentFrame]) {
-            SDL_RenderTexture(renderer, birdFrames[currentFrame], nullptr, &birdRect);
+        if (!birdFrames.empty() && birdFrames[tom.currentFrame]) {
+            SDL_FRect renderRect;
+            renderRect.x = tom.x;
+            renderRect.y = tom.y;
+            renderRect.w = tom.width;
+            renderRect.h = tom.height; 
+            SDL_RenderTexture(renderer, birdFrames[tom.currentFrame], nullptr, &renderRect);
         }
 
         SDL_RenderPresent(renderer);
